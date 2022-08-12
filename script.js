3bf797383bbcd4488b39b60c7f55770f15ac6b80
Prince Roy <princeku.ray143@gmail.com>
2022-08-12 19:30:12 +0530

    create func to render form on map click

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
 if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(
         pos => {
@@ -20,32 +21,52 @@ if (navigator.geolocation)
 
             const coords = [latitude, longitude];
 
-            const map = L.map('map').setView(coords, 13);
+            map = L.map('map').setView(coords, 13);
 
             L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
                 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
             }).addTo(map);
 
-            map.on('click', mapEvent => {
-                console.log(mapEvent);
-                const { lat, lng } = mapEvent.latlng;
-
-                L.marker([lat, lng])
-                    .addTo(map)
-                    .bindPopup(
-                        L.popup({
-                            maxWidth: 250,
-                            minWidth: 100,
-                            autoClose: false,
-                            closeOnClick: false,
-                            className: 'running-popup',
-                        })
-                    )
-                    .setPopupContent('Workout')
-                    .openPopup();
+            // Handling clicks on map
+            map.on('click', mapE => {
+                mapEvent = mapE;
+                form.classList.remove('hidden');
+                inputDistance.focus();
             });
         },
         () => {
             alert('Could not get your position');
         }
     );
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Clear input fields on after submitting
+    inputDistance.value =
+        inputDuration.value =
+        inputCadence.value =
+        inputElevation.value =
+        '';
+
+    // Display maps
+    const { lat, lng } = mapEvent.latlng;
+    L.marker([lat, lng])
+        .addTo(map)
+        .bindPopup(
+            L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup',
+            })
+        )
+        .setPopupContent('Workout')
+        .openPopup();
+});
+
+inputType.addEventListener('change', () => {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
