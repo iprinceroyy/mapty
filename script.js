580e59b2c5fac1efd5113150c70ae778cb670856
Prince Roy <princeku.ray143@gmail.com>
2022-08-12 23:43:12 +0530

    validtion check to newWorkout

--- script.js
+++ script.js
@@ -103,14 +103,40 @@ class App {
     _newWorkout(e) {
         e.preventDefault();
 
-        // Clear input fields on after submitting
-        inputDistance.value =
-            inputDuration.value =
-            inputCadence.value =
-            inputElevation.value =
-            '';
+        const isValid = (...inputs) =>
+            inputs.every(inp => {
+                Number.isFinite(inp);
+            });
 
-        // Display maps
+        const allPositive = (...inputs) => inputs.every(inp => inp > 0);
+
+        // Get data from form
+        const type = inputType.value;
+        const distance = +inputDistance.value;
+        const duration = +inputDuration.value;
+
+        // If workout is running, create running object
+        if (type === 'running') {
+            const cadence = +inputCadence.value;
+            // Check if data is valid
+            if (!isValid(distance, duration, cadence) ||
+                !allPositive(distance, duration, cadence)
+            ) {
+                return alert('Inputs have to be postive numbers');
+            }
+        }
+
+        // If workout is cycling, create cycling object
+        if (type === 'cycling') {
+            const elevation = +inputElevation.value;
+            if (!isValid(distance, duration, elevation) ||
+                !allPositive(distance, duration)
+            ) {
+                return alert('Inputs have to be postive numbers');
+            }
+        }
+
+        // Render workout on map as marker
         const { lat, lng } = this._mapEvent.latlng;
         L.marker([lat, lng])
             .addTo(this._map)
@@ -125,6 +151,17 @@ class App {
             )
             .setPopupContent('Workout')
             .openPopup();
+
+        // Render workout on list
+
+        // Hide formm + Clear input fields
+        inputDistance.value =
+            inputDuration.value =
+            inputCadence.value =
+            inputElevation.value =
+            '';
+
+        // Display maps
     }
 }
 
